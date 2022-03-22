e9606253fbfcaccf46454a9432758fb3fd734b81
Carolyn B <bmathematics@gmail.com>
2022-03-21 20:52:44 -0400

    added routes and models

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 const Sequelize = require('sequelize');
 
 //create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PW, {
+const sequelize = new Sequelize (process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PW, {
     host: 'localhost',
     dialect:'mysql',
     port: 3306
